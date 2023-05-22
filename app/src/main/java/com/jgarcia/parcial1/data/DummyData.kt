0bea5282ea82baf8ed41a328bb4120d2cefdc1af
Enrique García <54822214+enriquegarcia92@users.noreply.github.com>
2023-05-22 13:27:09 -0600

    Capa modelo

---
+++ app/src/main/java/com/jgarcia/parcial1/data/DummyData.kt
@@ -0,0 +1,5 @@
+package com.jgarcia.parcial1.data
+
+import com.jgarcia.parcial1.data.model.PackModel
+
+var pack = mutableListOf<PackModel>()
---
+++ app/src/main/java/com/jgarcia/parcial1/data/model/PackModel.kt
@@ -0,0 +1,6 @@
+package com.jgarcia.parcial1.data.model
+
+class PackModel(
+    val nombre: String,
+    val precio: String
+)
